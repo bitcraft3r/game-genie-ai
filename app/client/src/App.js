d99e9c42e4d4a0bd97e36ecba7a88acf7c047e19
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-01-23 17:22:17 +0800

    implement messages, add new form inputs

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
 import Header from './Components/Header';
-import Messages from './Components/Messages';
 import Input from './Components/Input';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Messages />
       <Input />
     </div>
   );
--- app/client/src/Components/Input.jsx
+++ app/client/src/Components/Input.jsx
@@ -3,24 +3,44 @@ import { useState } from 'react';
 
 const Input = () => {
     const [input, setInput] = useState("");
+    const [chatLog, setChatLog] = useState([
+        { user: "User", message: "What is this website about?"},
+        { user: "Aira", message: "Hi, I'm Aira, your personal AI assistant. Ask me any question to get started."},
+    ]);
     
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(input);
+        let chatLogNew = [...chatLog, {
+            user: "User", message: `${input}`
+        }]
+        setInput("");
+        setChatLog(chatLogNew);
     }
 
     return (
-        <div className="form-container">
-            <form onSubmit={handleSubmit}>
-                <input
-                    rows="1"
-                    value={input}
-                    placeholder="Ask a question"
-                    onChange={(e) => setInput(e.target.value)}
-                    className="form-input"
-                ></input>
-                <button>Submit</button>
-            </form>
+        <div>
+            <div className="messages-container">
+                <div className="messages-log">
+                    {chatLog.map((message, index) => {
+                        return (
+                            <div key={index}>{message.user}: {message.message}</div>
+                        )
+                    })}
+                </div>
+            </div>
+            <div className="form-container">
+                <form onSubmit={handleSubmit}>
+                    <input
+                        rows="1"
+                        value={input}
+                        placeholder="Ask a question"
+                        onChange={(e) => setInput(e.target.value)}
+                        className="form-input"
+                    ></input>
+                    <button>Submit</button>
+                </form>
+            </div>
         </div>
     )
 };
