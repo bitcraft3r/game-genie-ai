a40586cb9c96faca499d06526ce0f90640d82327
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-01-23 17:06:42 +0800

    implement submit form for Input component

--- app/client/src/Components/Input.jsx
+++ app/client/src/Components/Input.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { useState } from 'react';
 
 const Input = () => {
-  return (
-    <div>Input</div>
-  )
+    const [input, setInput] = useState("");
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
+    return (
+        <div className="form-container">
+            <form onSubmit={handleSubmit}>
+                <input
+                    rows="1"
+                    value={input}
+                    placeholder="Ask a question"
+                    onChange={(e) => setInput(e.target.value)}
+                    className="form-input"
+                ></input>
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 };
 
 export default Input;
