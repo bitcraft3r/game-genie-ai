7d367ace4e9e7e9ff4ce34d3c19fd0ae173fdf23
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-02-04 15:42:00 +0800

    refactor links to use array and map

--- app/client/src/Components/Create.jsx
+++ app/client/src/Components/Create.jsx
@@ -1,6 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+const itemsArr = [
+    { category: "brand", slug: "linkedin", title: "LinkedIn Bio" },
+    { category: "brand", slug: "twitter", title: "Twitter Bio" },
+    { category: "brand", slug: "marketing", title: "Marketing Copy" },
+    { category: "brand", slug: "elevator", title: "Elevator Pitch" },
+    { category: "copy", slug: "summary", title: "Summarize" },
+    { category: "copy", slug: "blog", title: "Blog Post" },
+    { category: "copy", slug: "thread", title: "Twitter Thread" },
+    { category: "copy", slug: "email", title: "Write Emails" },
+    { category: "copy", slug: "freestyle", title: "Freestyle Writer" },
+    { category: "hr", slug: "description", title: "Job Description" },
+    { category: "hr", slug: "interview", title: "Interview Questions" },
+];
+
+const brands = itemsArr.map((e, i) => {
+    if (e.category === "brand") 
+    return (
+        <div key={i+e.slug}>
+            <Link to={`/create/${e.category}/${e.slug}`}>{e.title}</Link>
+        </div>
+    )
+});
+
+const copies = itemsArr.map((e, i) => {
+    if (e.category === "copy") 
+    return (
+        <div key={e.category+i+e.slug}>
+            <Link to={`/create/${e.category}/${e.slug}`}>{e.title}</Link>
+        </div>
+    )
+});
+
+const hr = itemsArr.map((e, i) => {
+    if (e.category === "hr") 
+    return (
+        <div key={i+e.slug}>
+            <Link to={`/create/${e.category}/${e.slug}`}>{e.title}</Link>
+        </div>
+    )
+});
+
 const Create = () => {
   return (
     <div className="container">
@@ -15,23 +56,15 @@ const Create = () => {
         <div className="create-container">
             <div>
                 <h3><Link to="/create/brand">Branding</Link></h3>
-                <div><Link to="/create/brand/linkedin">LinkedIn Bio</Link></div>
-                <div><Link to="/create/brand/twitter">Twitter Bio</Link></div>
-                <div><Link to="/create/brand/marketing">Marketing Copy</Link></div>
-                <div><Link to="/create/brand/elevator">Elevator Pitch</Link></div>
+                <div>{brands}</div>
             </div>
             <div>
                 <h3><Link to="/create/copy">Copywriting</Link></h3>
-                <div><Link to="/create/copy/summarize">Summarize</Link></div>
-                <div><Link to="/create/copy/thread">Twitter Threads</Link></div>
-                <div><Link to="/create/copy/blog">Blog Post</Link></div>
-                <div><Link to="/create/copy/email">Write Emails</Link></div>
-                <div><Link to="/create/copy/grammar">Grammar Check</Link></div>
+                <div>{copies}</div>
             </div>
             <div>
                 <h3><Link to="/create/hr">HR</Link></h3>
-                <div><Link to="/create/hr/description">Job Description</Link></div>
-                <div><Link to="/create/hr/interview">Interview Questions</Link></div>
+                <div>{hr}</div>
             </div>
         </div>
     </div>
