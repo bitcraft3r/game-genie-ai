6b91e3fa61686062341c00ce0989c835184fec99
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-02-09 11:26:17 +0800

    add index.js in components for easy access

---
+++ app/client/src/Components/index.js
@@ -0,0 +1,27 @@
+import Account from './Account';
+import Chat from './Chat';
+import Craft from './Craft';
+import CSV from './CSV';
+import Dream from './Dream';
+import Form from './Form';
+import Home from './Home';
+import Item from './Item';
+import Navbar from './Navbar';
+import Protected from './Protected';
+import Section from './Section';
+import Signin from './Protected';
+
+export {
+    Account,
+    Chat,
+    Craft,
+    CSV,
+    Dream,
+    Form,
+    Home,
+    Item,
+    Navbar,
+    Protected,
+    Section,
+    Signin,
+};
