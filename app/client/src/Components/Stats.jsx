b65d84b605d5ebf69fc6a6733888044dd78ccb8e
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-02-10 01:28:23 +0800

    update Stats page

--- app/client/src/Components/Stats.jsx
+++ app/client/src/Components/Stats.jsx
@@ -5,6 +5,9 @@ import { doc, getDoc } from 'firebase/firestore'
 const Stats = () => {
     const [countCSV, setCountCSV] = useState();
     const [countPNG, setCountPNG] = useState();
+    const [countChat, setCountChat] = useState();
+    const [countDream, setCountDream] = useState();
+    const [countDice, setCountDice] = useState(); 
     const docRef = doc(db, "statistics", "downloads");
 
     useEffect(() => {
@@ -21,6 +24,9 @@ const Stats = () => {
 
             setCountCSV(docSnap.data().countCSV);
             setCountPNG(docSnap.data().countPNG);
+            setCountChat(docSnap.data().countChat);
+            setCountDream(docSnap.data().countDream);
+            setCountDice(docSnap.data().countDice);
         }
         getStats();
     }, []);
@@ -30,6 +36,9 @@ const Stats = () => {
             <h1>Statistics</h1>
             <h2>Download stats</h2>
             <div className="container">
+                <p>Chats Generated: {countChat}</p>
+                <p>Images Generated: {countDream}</p>
+                <p>Random PFPs Generated: {countDice}</p>
                 <p>CSV Downloaded: {countCSV}</p>
                 <p>PNG Downloaded: {countPNG}</p>
             </div>
