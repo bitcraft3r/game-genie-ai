4ca03647150689d7f5adfcd59826c831117187a0
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-02-09 21:16:47 +0800

    Update footer

--- app/client/src/Components/Footer.jsx
+++ app/client/src/Components/Footer.jsx
@@ -4,7 +4,9 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <div className="footer">
+            <div>Copyright © GameGenieAI.com</div>
             <Link to="/stats">Statistics</Link>
+            <p>v0.1.0</p>
         </div>
     )
 }
