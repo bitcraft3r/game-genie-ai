12c46f68fc9751ec069700f7bcae019f13e05729
SOV3333 <8282076+sov3333@users.noreply.github.com>
2023-02-18 16:09:19 +0800

    add google analytics and gtm id

--- app/client/pages/_app.js
+++ app/client/pages/_app.js
@@ -1,7 +1,27 @@
 import '../styles/styles.css';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-5QNSDWF2FV"/>
+        <Script
+            id='google-analytics'
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-5QNSDWF2FV', {
+                page_path: window.location.pathname,
+                });
+                `,
+            }}
+        />
+        <Component {...pageProps} />
+        </>
+    );
 }
 
 export default MyApp;
