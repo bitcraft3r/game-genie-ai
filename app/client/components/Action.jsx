7f35ab700de8723b1e0a281dadb1cc5dd496776a
sov3333 <8282076+sov3333@users.noreply.github.com>
2023-02-21 18:30:50 +0800

    add personal stats to account page
    todo: fix wrong stats

--- app/client/components/Action.jsx
+++ app/client/components/Action.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { db } from '../firebase/clientApp';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth, db } from '../firebase/clientApp';
 import { collection, addDoc, Timestamp } from 'firebase/firestore';
 // import { useLocation } from 'react-router';
-// import { UserAuth } from '../context/AuthContext';
 
 /**
  * 
@@ -14,18 +14,17 @@ const Action = ({ type, action, prompt, handleParentSubmit, buttonName }) => {
 
     // const location = useLocation();
     // const locationPathname = location.pathname;
-    // const { user } = UserAuth();
 
-    // let userName = user?.displayName || "anonymous";
-    // let userEmail = user?.email || "-"
+    const [user, setUser] = useAuthState(auth);
+
+    let userName = user?.displayName || "anonymous";
+    let userEmail = user?.email || "-"
 
     const addAction = async () => {
         // console.log(`creating action on firestore`);
         const docRef = await addDoc(collection(db, "actions"), {
-            // name: userName,
-            // email: userEmail,
-            name: "test user name",
-            email: "test user email",
+            name: userName,
+            email: userEmail,
             action: action, // generate or download
             type: type, // text or image
             prompt: prompt,
--- app/client/pages/account.js
+++ app/client/pages/account.js
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
 import { signOut } from 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase/clientApp';
+import { auth, db } from '../firebase/clientApp';
+import { collection, query, where, getDocs } from 'firebase/firestore';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 
@@ -8,6 +10,105 @@ import Navbar from '../components/Navbar';
 const Account = () => {
 
     const [user, setUser] = useAuthState(auth);
+    console.log(`user is:`, user)
+
+    const [isLoading, setIsLoading] = useState(false);
+
+    /**
+     * Bulk of code on this Component is for 
+     * Getting the relevant user's actions data from Firestore
+     * 
+     */
+
+    // constants for (A)
+    const [countUserActions, setCountUserActions] = useState(0);
+
+    // constants for (B)
+    const [countUserGenerated, setCountUserGenerated] = useState(0);
+    const [countUserDownloaded, setCountUserDownloaded] = useState(0);
+    const [countUserTweeted, setCountUserTweeted] = useState(0);
+    const [countUserImage, setCountUserImage] = useState(0);
+    const [countUserText, setCountUserText] = useState(0);
+
+    // constants for (C)
+    const [countUserGeneratedImage, setCountUserGeneratedImage] = useState(0);
+    const [countUserGeneratedText, setCountUserGeneratedText] = useState(0);
+    const [countUserDownloadedImage, setCountUserDownloadedImage] = useState(0);
+    const [countUserDownloadedText, setCountUserDownloadedText] = useState(0);
+
+    const actionsRef = collection(db, "actions");
+
+    // (A): total user's actions
+    const qUserActions = query(actionsRef, where("email", "==", user.email));
+
+    // (B): total generated, total downloaded, total image actions, total text actions
+    const qUserGenerated = query(actionsRef, where("email", "==", user.email), where("action", "==", "generate"));
+    const qUserDownloaded = query(actionsRef, where("email", "==", user.email), where("action", "==", "download"));
+    const qUserTweeted = query(actionsRef, where("email", "==", user.email), where("type", "==", "tweet"));
+    const qUserImage = query(actionsRef, where("email", "==", user.email), where("type", "==", "image"));
+    const qUserText = query(actionsRef, where("email", "==", user.email), where("type", "==", "text"));
+
+    // (C): total image generated, total text generated, total image downloaded, total text downloaded
+    const qUserGeneratedImage = query(actionsRef, where("email", "==", user.email), where("action", "==", "generate"), where("type", "==", "image"));
+    const qUserGeneratedText = query(actionsRef, where("email", "==", user.email), where("action", "==", "generate"), where("type", "==", "text"));
+    const qUserDownloadedImage = query(actionsRef, where("email", "==", user.email), where("action", "==", "download"), where("type", "==", "image"));
+    const qUserDownloadedText = query(actionsRef, where("email", "==", user.email), where("action", "==", "download"), where("type", "==", "text"));
+
+    useEffect(() => {
+        const getStats = async () => {
+            setIsLoading(true);
+
+            // Read and increment counters for (A)
+            const queryUserActionsSnapshot = await getDocs(qUserActions);
+            queryUserActionsSnapshot.forEach((doc) => {
+                console.log(doc.id, " => ", doc.data());
+                setCountUserActions(countUserActions => countUserActions+1);
+            });
+
+            // Read and increment counters for (B)
+            const queryUserGeneratedSnapshot = await getDocs(qUserGenerated);
+            queryUserGeneratedSnapshot.forEach((doc) => {
+                setCountUserGenerated(countUserGenerated => countUserGenerated+1);
+            });
+            const queryUserDownloadedSnapshot = await getDocs(qUserDownloaded);
+            queryUserDownloadedSnapshot.forEach((doc) => {
+                setCountUserDownloaded(countUserDownloaded => countUserDownloaded+1);
+            });
+            const queryUserTweetedSnapshot = await getDocs(qUserTweeted);
+            queryUserTweetedSnapshot.forEach((doc) => {
+                setCountUserTweeted(countUserTweeted => countUserTweeted+1);
+            });
+            const queryUserImageSnapshot = await getDocs(qUserImage);
+            queryUserImageSnapshot.forEach((doc) => {
+                setCountUserImage(countUserImage => countUserImage+1);
+            });
+            const queryUserTextSnapshot = await getDocs(qUserText);
+            queryUserTextSnapshot.forEach((doc) => {
+                setCountUserText(countUserText => countUserText+1);
+            });
+
+            // Read and increment counters for (C)
+            const queryUserGeneratedImageSnapshot = await getDocs(qUserGeneratedImage);
+            queryUserGeneratedImageSnapshot.forEach((doc) => {
+                setCountUserGeneratedImage(countUserGeneratedImage => countUserGeneratedImage+1);
+            });
+            const queryUserGeneratedTextSnapshot = await getDocs(qUserGeneratedText);
+            queryUserGeneratedTextSnapshot.forEach((doc) => {
+                setCountUserGeneratedText(countUserGeneratedText => countUserGeneratedText+1);
+            });
+            const queryUserDownloadedImageSnapshot = await getDocs(qUserDownloadedImage);
+            queryUserDownloadedImageSnapshot.forEach((doc) => {
+                setCountUserDownloadedImage(countUserDownloadedImage => countUserDownloadedImage+1);
+            });
+            const queryUserDownloadedTextSnapshot = await getDocs(qUserDownloadedText);
+            queryUserDownloadedTextSnapshot.forEach((doc) => {
+                setCountUserDownloadedText(countUserDownloadedText => countUserDownloadedText+1);
+            });
+
+            setIsLoading(false);
+        }
+        getStats();
+    }, [])
 
     const handleSignOut = async () => {
         try {
@@ -34,7 +135,7 @@ const Account = () => {
                 userBadge2={Math.ceil(countUserDownloaded/2)}
                 userBadge3={Math.ceil(countUserActions/2)}
             />
-        </div>
+        </div> */}
         <div className="container">
             <h3>Your Stats</h3>
             {isLoading ? (
@@ -64,7 +165,7 @@ const Account = () => {
                 </div>
                 </>
             )}
-        </div> */}
+        </div>
     </div>
     <Footer />
     </>
